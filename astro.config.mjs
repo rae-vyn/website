81c482efd33eb870d30c068fdd567d4bdb5880bc
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2024-07-20 11:41:42 -0400

    refactored site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    adapter: netlify(),
+  adapter: netlify(),
+  integrations: [mdx()]
 });
