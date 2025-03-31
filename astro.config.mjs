7c4760222546a3c2d0618ac2054fc10a404a0b3d
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2025-03-31 14:10:11 -0400

    started work on Balatro tutorials

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,19 @@ import netlify from '@astrojs/netlify';
 
 import mdx from "@astrojs/mdx";
 
+import { astroExpressiveCode } from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  integrations: [mdx()]
+  integrations: [astroExpressiveCode({
+    themes: ['catppuccin-mocha', 'catppuccin-latte'],
+    styleOverrides: {
+      textMarkers: {
+        markHue: '310',
+        borderOpacity: '75%',
+        backgroundOpacity: '15%'
+      }
+    }
+  }), mdx()],
 });
